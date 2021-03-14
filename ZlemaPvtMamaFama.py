d00a200c21a8564df077e38d54cc7f66249a625b
Marco Hung <37731008+marcohung818@users.noreply.github.com>
2021-03-15 01:19:38 +0800

    PVT problem

--- ZlemaPvtMamaFama.py
+++ ZlemaPvtMamaFama.py
@@ -1,29 +1,36 @@
 import yfinance as yf
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 def main():
-    #pd.options.display.max_rows = None
     sysmbol = input("Input Sysmbol: ")
-    stockHistory = getStockPrice(sysmbol)
-    zlema(stockHistory)
-    #stockHistory['Close'].plot()
-    #plt.xlabel("Data")
-    #plt.ylabel("$ price")
-    #plt.show()
+    stock = yf.Ticker(sysmbol)
+    stockHistory = getStockPrice(stock)
+    zlemabuysell = zlema(stockHistory)
+    #print(zlemabuysell)
+    #pvtbuysell = pvt(stockHistory)
+    #mafabuysell = mafa(stockHistory)
     return
 
-def getStockPrice(sysmbol):
-    shock = yf.Ticker(sysmbol)
-    price = shock.history(period="5y")
+def getStockPrice(stock):
+    price = stock.history(period="5y")
     return price
 
-def zlema(data):
+def zlema(data):#Using Close to calculate
     period = 14
     lag = round((period - 1) / 2)
     data['shift'] = data['Close'] + (data['Close'] - data['Close'].shift(lag))
+    data['ZLEMA'] = data['shift'].ewm(span=period, adjust=False).mean()
+    data['ZLEMABuySell'] = np.where(data['ZLEMA'] >= data['ZLEMA'].shift(1), True, False)
     data = data[data['shift'].notna()]
-    data['ZLEMA'] = data['shift'].ewm(span=lag).mean()
-    print(data['ZLEMA'])
+    data = data.iloc[1:]
+    print(data)
+    return data['ZLEMABuySell']
 
+def pvt(data):#Using Volume to calculate
+    print(data['Volume'])
+    return
+
+pd.options.display.max_rows = None
 main()
