4384d5f00ea37d14a002784eb6c4e1ef49cf77f0
Marco Hung <37731008+marcohung818@users.noreply.github.com>
2021-03-20 11:07:45 +0800

    Add files via upload

--- ZlemaPvtMamaFama.py
+++ ZlemaPvtMamaFama.py
@@ -5,32 +5,41 @@ import numpy as np
 
 def main():
     sysmbol = input("Input Sysmbol: ")
-    stock = yf.Ticker(sysmbol)
-    stockHistory = getStockPrice(stock)
-    zlemabuysell = zlema(stockHistory)
-    #print(zlemabuysell)
-    #pvtbuysell = pvt(stockHistory)
+    #stock = yf.Ticker(sysmbol)
+    stockHistory = getStockPrice(sysmbol)
+    #zlemabuysell = zlema(stockHistory)
+    pvtbuysell = pvt(stockHistory)
     #mafabuysell = mafa(stockHistory)
     return
 
 def getStockPrice(stock):
-    price = stock.history(period="5y")
-    return price
+    stockPriceData = yf.download(stock, period="max", interval="1d")
+    stockPriceData = stockPriceData.round(4)
+    #print(stockPriceData)
+    return stockPriceData
 
 def zlema(data):#Using Close to calculate
     period = 14
     lag = round((period - 1) / 2)
-    data['shift'] = data['Close'] + (data['Close'] - data['Close'].shift(lag))
-    data['ZLEMA'] = data['shift'].ewm(span=period, adjust=False).mean()
+    data['Shift'] = data['Close'] + (data['Close'] - data['Close'].shift(lag))
+    data['ZLEMA'] = data['Shift'].ewm(span=period, adjust=False).mean()
     data['ZLEMABuySell'] = np.where(data['ZLEMA'] >= data['ZLEMA'].shift(1), True, False)
-    data = data[data['shift'].notna()]
-    data = data.iloc[1:]
-    print(data)
+    data = data[data['ZLEMA'].notna()]
     return data['ZLEMABuySell']
 
 def pvt(data):#Using Volume to calculate
-    print(data['Volume'])
-    return
+    signalPeriod = 21
+    data['CloseShift1'] = data["Close"].shift(1)
+    data['Pre_PVT'] = ((data['Close'] - data['CloseShift1']) / data['CloseShift1']) * data['Volume']
+    data['PVT'] = data['Pre_PVT'].cumsum()
+    data['Signal'] = data['PVT'].ewm(span=signalPeriod, adjust=False).mean()
+    data['Signal_plus1day'] = data['Signal'].shift(-1)
+    data['Signal_minus1day'] = data['Signal'].shift(1)
+    data = data[['Close', 'CloseShift1', 'Volume','Pre_PVT', 'PVT', 'Signal', 'Signal_plus1day', 'Signal_minus1day']]
+    data = data[data['PVT'].notna()]
 
-pd.options.display.max_rows = None
+    print(data)
+    return data
+
+#pd.options.display.max_rows = None
 main()
